c87214224886c645a03b9a0757eb44e30fecb160
TimPaler <paler.tim@gmail.com>
2022-11-28 09:21:55 +0600

    Upload

---
+++ app/src/main/java/com/example/das06_lab3/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.das06_lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onNext(View v) {
+        Intent intent = new Intent(MainActivity.this, second_activity.class);
+        startActivity(intent);
+    }
+    public void onExit(View v) {
+        finishAffinity();
+    }
+}
